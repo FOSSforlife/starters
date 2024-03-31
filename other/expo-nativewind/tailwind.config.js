4ef8b76e75421856f6140a78140ac13a55cd17df
Elias Jackson <ejthedj0@gmail.com>
2024-03-31 12:12:55 -0700

    Add nativecn-ui

--- other/expo-nativewind/tailwind.config.js
+++ other/expo-nativewind/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
