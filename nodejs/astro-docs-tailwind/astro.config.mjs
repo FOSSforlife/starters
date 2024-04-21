81eb58a3b3df80f743420ef177d409f0d1b90bef
Elias Jackson <ejthedj0@gmail.com>
2024-04-21 13:59:32 -0700

    Rename astro-docs-tailwind

---
+++ nodejs/astro-docs-tailwind/astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      autogenerate: {
+        directory: 'guides'
+      }
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind()]
+});
