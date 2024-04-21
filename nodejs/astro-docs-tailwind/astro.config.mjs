1625514e71f847d0e37365351c0bfb9f2bf77a4a
Elias Jackson <ejthedj0@gmail.com>
2024-04-21 15:39:41 -0700

    Add red bull link to astro docs

--- nodejs/astro-docs-tailwind/astro.config.mjs
+++ nodejs/astro-docs-tailwind/astro.config.mjs
@@ -1,25 +1,34 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'My Docs',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Guides',
-      autogenerate: {
-        directory: 'guides'
-      }
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), tailwind()]
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/fossforlife',
+      },
+      components: {
+        ThemeSelect: './src/components/ThemeSelect.astro',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: {
+            directory: 'guides',
+          },
+        },
+        {
+          label: 'Reference',
+          autogenerate: {
+            directory: 'reference',
+          },
+        },
+      ],
+    }),
+    tailwind(),
+  ],
 });
